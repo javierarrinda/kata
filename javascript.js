85cb254bde8a0530e651ed35037bffceed4eb864
Javier <jarrindaulivi4@gmail.com>
2018-11-10 12:03:14 -0500

    saturday codewars mathematical operations

--- javascript.js
+++ javascript.js
@@ -68,3 +68,20 @@ function lastDayIsFriday(initialYear, endYear = initialYear) {
   }
   return fridays
 }
+
+//question saturday
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+//answer
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case '+': return value1 + value2;
+    case '-': return value1 - value2;
+    case '*': return value1 * value2;
+    case '/': return value1 / value2;
+    default: return 'Operation must be one of + - * /';
+  }
+}
